943be2331b27d37832ea5d6c64a179931a59b9f2
Salvatore Pruiti (maghetto85) <salvatorepruiti@hotmail.com>
2015-04-05 16:52:38 +0200

    Initial Release

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+/*    Copyright (C) 2015 Pruiti Salvatore - All Rights Reserved
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+    $rt = (isset($_GET['rt']) ? $rt = $_GET['rt'] : 'index');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>FAST MVC FRAMEWORK</title>
+    </head>
+    <body>
+        <h2>Coming Soon!</h2>
+        <p>Requested Page: <?php echo $rt; ?></p>
+    </body>
+</html>
+
+
